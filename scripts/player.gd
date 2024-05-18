55db441183111db789a13a9937bd1d1a6ce745ff
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2024-05-18 15:49:19 -0300

    update: add player jumps and run animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,7 +5,7 @@ const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -13,12 +13,27 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	# Get the input direction (-1, 0, 1)
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	# Flip sprites
+	if direction > 0:
+		sprite.flip_h = false
+	elif direction < 0:
+		sprite.flip_h = true
+		
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			sprite.play("default")
+		else:
+			sprite.play("run")
+	else:
+		sprite.play("jump")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
